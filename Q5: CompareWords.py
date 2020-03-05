b520f2d6724b4ff572442606bacafe9543799b6f
alexandrehuynh <36118582+alexandrehuynh@users.noreply.github.com>
2020-03-05 12:14:01 -0800

    Clean code for Q5

--- Q5: CompareWords.py
+++ Q5: CompareWords.py
@@ -3,12 +3,13 @@ import sys
 word1 = str(input("Enter full word: "))
 word2 = str(input("Enter partial string: "))
 
-def CompareWords(word1, word2):
+
+def compare_words(w1, w2):
     comp_words = word2 in word1  # Check if the word1 is apart of word2 (case sensitive)
     return comp_words
 
 
-compResults = CompareWords(word1, word2)
+compResults = compare_words(word1, word2)
 binaryVal = int(compResults)  # Convert boolean function to int
 
 
