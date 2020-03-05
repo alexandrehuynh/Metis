66b9f322c7cda6412d05ddb9dcf810737750514a
alexandrehuynh <36118582+alexandrehuynh@users.noreply.github.com>
2020-03-05 12:18:29 -0800

    Clean code for Q1

--- Q1: ReversewithA.py
+++ Q1: ReversewithA.py
@@ -8,12 +8,13 @@ def rev_function(x):
     return x[::-1]  # Return last char first
 
 
+#  Reverse phrase and insert A between words
 rev_phrase = rev_function(phrase)  # Reverse phrase
 rev_phrase = rev_phrase.split(" ")  # Split strings into list
 phrase_A = 'A'.join(rev_phrase)  # Join string with A in between
 
+#  Insert A to front and end of list
 new_A_phrase = phrase_A.split(" ")  # Split into list
-
 new_A_phrase.insert(0, insert_var)  # Add "A" in the front
 new_A_phrase.append(insert_var)  # Add "A" at the end
 final_phrase = ''.join(new_A_phrase)  # Join everything together
