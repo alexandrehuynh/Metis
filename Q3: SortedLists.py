e9a4ebee2b078b934fbed40e110534cbc985309d
alexandrehuynh <36118582+alexandrehuynh@users.noreply.github.com>
2020-03-05 12:13:44 -0800

    Clean code for Q3

--- Q3: SortedLists.py
+++ Q3: SortedLists.py
@@ -5,11 +5,13 @@ list1 = user_input1.split(" ")
 user_input2 = input("Enter List2 with space in between numbers: ")
 list2 = user_input2.split(" ")
 
-def Diff(list1, list2):
+
+def diff(l1, l2):
     return[x for x in list2 if x not in list1]  # Check values in List2 if in List1, returns difference
 
-wholeSet = sorted(list1+list2)
-difSet = Diff(list1, list2)
-resultSet = Diff(difSet, wholeSet)
+
+wholeSet = sorted(list1 + list2)
+difSet = diff(list1, list2)
+resultSet = diff(difSet, wholeSet)
 
 print(resultSet)
